887038e65f76e4344d31c7881e03ac1e6c88e5f1
Wolfserk <vvolfserk@gmail.com>
2025-05-23 18:26:46 +0400

    Добавьте файлы проекта.

---
+++ TaskManagementSystem.Domain/Entities/TaskItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagementSystem.Domain.Entities
+{
+    internal class TaskItem
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; } = null!;
+        public string? Description { get; set; }
+
+        public Enums.TaskStatus Status { get; set; } = Enums.TaskStatus.New;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; }
+        public DateTime? DueDate { get; set; }
+
+        public Guid? UserId { get; set; }
+        public User? User { get; set; }
+    }
+}
---
+++ TaskManagementSystem.Domain/Entities/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagementSystem.Domain.Entities
+{
+    internal class User
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Email { get; set; } = null!;
+
+        public List<TaskItem> Tasks { get; set; } = [];
+    }
+}
